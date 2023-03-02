151b3be58989dd891a2faa57e5314bf8ed3751c1
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-03-02 23:47:48 +0800

    .

--- NUS Stuff/IT2002/Lectures/2. Creating and Populating Tables with Constraints/Code/Lecture2.sql
+++ NUS Stuff/IT2002/Lectures/2. Creating and Populating Tables with Constraints/Code/Lecture2.sql
@@ -11,24 +11,24 @@ CREATE TABLE downloads(
     price NUMERIC);
 
 CREATE TABLE customers (
-	first_name VARCHAR(64),
-	last_name VARCHAR(64),
-	email VARCHAR(64),
-	dob DATE,
-	since DATE,
-	customerid VARCHAR(16),
-	country VARCHAR(16));
+    first_name VARCHAR(64),
+    last_name VARCHAR(64),
+    email VARCHAR(64),
+    dob DATE,
+    since DATE,
+    customerid VARCHAR(16),
+    country VARCHAR(16));
 
 CREATE TABLE games (
-	name VARCHAR(32),
-	version CHAR(3),
-	price DECIMAL(2,2));
-	
+    name VARCHAR(32),
+    version CHAR(3),
+    price DECIMAL(2,2));
+
 CREATE TABLE downloads(
-	customerid VARCHAR(16),
-	name VARCHAR(32),
-	version CHAR(3));
-	
+    customerid VARCHAR(16),
+    name VARCHAR(32),
+    version CHAR(3));
+
 
 INSERT INTO downloads VALUES("...", "..."...)
 
