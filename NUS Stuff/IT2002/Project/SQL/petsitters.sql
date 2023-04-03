fd04920fc8c4bedaf4a8c281c5a4aeefdfaf5ee3
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2023-04-04 07:09:56 +0800

    .

---
+++ NUS Stuff/IT2002/Project/SQL/petsitters.sql
@@ -0,0 +1,8 @@
+INSERT INTO USERS VALUES ('jiaxin', 'jiaxin@gmail.com', '1234');
+INSERT INTO USERS VALUES ('admin', 'admin@gmail.com', '1234');
+
+
+INSERT INTO petsitters (username, startDate, endDate, price, dog, cat, petBoarding, dogWalking, petGrooming, petDaycare, petSitting, petTaxi) 
+VALUES ('admin','03-01-2023','03-08-2023','5','true','true','true','true','false','false','false','true');
+INSERT INTO petsitters (username, startDate, endDate, price, dog, cat, petBoarding, dogWalking, petGrooming, petDaycare, petSitting, petTaxi) 
+VALUES ('jiaxin','05-05-2023','08-08-2023','10','false','true','true','false','true','true','true','true');
